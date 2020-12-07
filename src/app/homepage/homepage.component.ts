2ba5f0d3d638ae5162e483c34526b896a0648476
Hanson Chen <hansonerschen@gmail.com>
2020-12-07 05:21:04 -0500

    Updated text.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,8 +9,8 @@ export class HomepageComponent implements OnInit {
   list = ['Hanson Chen',
   'Incoming SDE Intern @ Amazon | Summer \'21',
   'Software Developer Intern @ Magnet Forensics | Fall \'20',
-  'Software Developer Intern @ RBC Financial Group | Winter \'20',
-  'Software Developer Intern @ RBC Financial Group | Winter \'19',
+  'Software Developer Intern @ RBC | Winter \'20',
+  'Software Developer Intern @ RBC | Winter \'19',
   'BCS @ University of Waterloo \'21',
   'BBA @ Wilfrid Laurier University \'21'
 ];
