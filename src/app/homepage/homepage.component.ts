567041ff1b6e62158a6eed08bc3012e4a446ef43
Hanson Chen <hansonerschen@gmail.com>
2020-12-08 20:51:33 -0500

    Updated page.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent implements OnInit {
-  list = ['Hanson Chen',
-  'Incoming SDE Intern @ Amazon | Summer \'21',
-  'Software Developer Intern @ Magnet Forensics | Fall \'20',
-  'Software Developer Intern @ RBC | Winter \'20',
-  'Software Developer Intern @ RBC | Winter \'19',
-  'BCS @ University of Waterloo \'21',
-  'BBA @ Wilfrid Laurier University \'21'
-];
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
-
+export class HomepageComponent {
+  list = [
+    'Hanson Chen',
+    'Employment History:',
+    'Incoming SDE Intern @ Amazon | Summer \'21',
+    'Software Developer Intern @ Magnet Forensics | Fall \'20',
+    'Software Developer Intern @ RBC | Winter \'20',
+    'Software Developer Intern @ RBC | and also Winter \'19',
+    'BCS @ University of Waterloo \'21',
+    'BBA @ Wilfrid Laurier University \'21',
+    'github.com/hansoners'
+  ];
+  constructor() {}
 }
